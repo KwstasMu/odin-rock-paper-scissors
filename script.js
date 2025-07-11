a18727d5ae9d1a631d2e41e3aae565ea9d874abb
KwstasMu <kwstasmu@hotmail.com>
2025-07-11 16:31:07 +0300

    add some style and the ability to replay

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ let round = 0;
 
 const roundText = document.querySelector('#round');
 const results = document.querySelector('#results');
+const replay = document.querySelector('#replay');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(btn => btn.addEventListener('click', (e) => playRound(e.target.id, getComputerChoice())))
+
+replay.addEventListener('click', () => playAgain())
 
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
-}
-
-function getHumanChoice() {
-  const input = prompt("rock, paper or scissors?");
-  return input;
 }
 
 function playRound(humanChoice, computerChoice) {
@@ -31,20 +32,29 @@ function playRound(humanChoice, computerChoice) {
   }  
   roundText.textContent = `Round: ${++round}`
   checkWinner()
+  if (checkWinner()) {
+    btns.forEach(btn => btn.disabled = true)
+    replay.style.display = 'block';
+  }
 }
 
 
 function checkWinner() {
   if (humanScore === 5) {
       results.textContent = `Congratulations you won ${humanScore} to ${computerScore}`;
-      return;
+      return true;
     } else if (computerScore === 5) {
       results.textContent = `Sorry you lost ${computerScore} to ${humanScore}`;
-      return;
+      return true;
     }
   }
 
-const btns = document.querySelectorAll('.btn');
-btns.forEach(btn  => {
-  btn.addEventListener('click', (e) => playRound(e.target.value, getComputerChoice()))
-})
+function playAgain() {
+  btns.forEach(btn => btn.disabled = false);
+  replay.style.display = 'none';
+  humanScore = 0;
+  computerScore = 0;
+  round = 0;
+  roundText.textContent = '';
+  results.textContent = '';
+}
